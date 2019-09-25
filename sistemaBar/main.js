b0a1513c0765fa89acf8a4a737309f3d202ab7fb
giovi7 <giovi7azul@gmail.com>
2019-09-25 17:39:26 -0400

    uso de set y get

--- sistemaBar/main.js
+++ sistemaBar/main.js
@@ -5,6 +5,29 @@ var Bar = /** @class */ (function () {
         this.nombre = nom;
         this.capacidad = cap;
     }
+    /**
+     * Cambiar el valor de un atributo
+     * Creo el metodo set que recibe como parametro
+     * el valor que deseo actualizar
+     */
+    /**
+     * A una propiedad podemos asociarle un metodo llamado set en el momento que se le asigne un valor y otro metodo
+     * llamado get cuando se accede al contenido de la propiedad.
+     *
+     * Estos mètodos son opcionales y nos permiten validar el dato a signar a la propiedad o el valor de retorno
+     */
+    Bar.prototype.setNombre = function (nom) {
+        this.nombre = nom;
+    };
+    Bar.prototype.setCapacidad = function (cap) {
+        this.capacidad = cap;
+    };
+    Bar.prototype.getNombre = function () {
+        return this.nombre;
+    };
+    Bar.prototype.getCapacidad = function () {
+        return this.capacidad;
+    };
     return Bar;
 }());
 exports.Bar = Bar;
@@ -13,6 +36,18 @@ var BarMan = /** @class */ (function () {
         this.nombre = nom;
         this.apellido = apell;
     }
+    BarMan.prototype.setNombre = function (nom) {
+        this.nombre = nom;
+    };
+    BarMan.prototype.setApellido = function (apell) {
+        this.apellido = apell;
+    };
+    BarMan.prototype.getNombre = function () {
+        return this.nombre;
+    };
+    BarMan.prototype.getApellido = function () {
+        return this.apellido;
+    };
     return BarMan;
 }());
 exports.BarMan = BarMan;
@@ -22,6 +57,24 @@ var Trago = /** @class */ (function () {
         this.color = col;
         this.cantidad = cant;
     }
+    Trago.prototype.setNombre = function (nom) {
+        this.nombre = nom;
+    };
+    Trago.prototype.setColor = function (col) {
+        this.color = col;
+    };
+    Trago.prototype.setCantidad = function (cant) {
+        this.cantidad = cant;
+    };
+    Trago.prototype.getNombre = function () {
+        return this.nombre;
+    };
+    Trago.prototype.getColor = function () {
+        return this.color;
+    };
+    Trago.prototype.getCantidad = function () {
+        return this.cantidad;
+    };
     return Trago;
 }());
 exports.Trago = Trago;
@@ -39,6 +92,23 @@ var bar1 = new Bar("nueva era", 50);
 var bar2 = new Bar("el patio", 200);
 var barman1 = new BarMan("julio", "perez");
 var trago1 = new Trago("mojito", "rojo", 1);
+bar1.setNombre("El pueblito");
+// bar1.nombre = "pueblito"; forma incorrecta actualizar un atributo
+console.log(bar1);
+// console.log(bar1.nombre);//forma incorrecta de pedir valor
+console.log(bar1.getNombre());
+bar1.setCapacidad(70);
+console.log(bar1.getCapacidad());
+barman1.setNombre("Carlos");
+console.log(barman1.getNombre());
+barman1.setApellido("Alegre");
+console.log(barman1.getApellido());
+trago1.setNombre("jugo");
+console.log(trago1.getNombre());
+trago1.setColor("azul");
+console.log(trago1.getColor());
+trago1.setCantidad(7);
+console.log(trago1.getCantidad());
 console.log(bar1);
 console.log(bar2);
 console.log(barman1);
--- sistemaBar/main.ts
+++ sistemaBar/main.ts
@@ -1,29 +1,108 @@
 export class Bar {
-    nombre:string; // atributo
-    capacidad: number; //atributo
-    constructor (nom: string, cap:number ){
+    private nombre:string; // atributo
+    private capacidad: number; //atributo
+    constructor (nom: string, cap: number ){
         this.nombre = nom;
         this.capacidad = cap;
     }
+   /**
+    * Cambiar el valor de un atributo
+    * Creo el metodo set que recibe como parametro
+    * el valor que deseo actualizar
+    */
+
+    /**
+     * A una propiedad podemos asociarle un metodo llamado set en el momento que se le asigne un valor y otro metodo
+     * llamado get cuando se accede al contenido de la propiedad.
+     * 
+     * Estos mètodos son opcionales y nos permiten validar el dato a signar a la propiedad o el valor de retorno
+     */
+
+    public setNombre(nom: string): void {
+        this.nombre = nom;
+    }
+    
+    public setCapacidad(cap : number) {
+        this.capacidad = cap;
+    }
+    
+    public getNombre(): string {
+        return this.nombre;
+    } 
+    
+    public getCapacidad() : number {
+        return this.capacidad;
+    }
+       
 }
 export class BarMan {
-    nombre: string;
-    apellido: string;
-    constructor(nom: string, apell:string){
-        this.nombre = nom
+    private nombre: string;
+    private apellido: string;
+    constructor(nom: string, apell: string){
+        this.nombre = nom;
+        this.apellido = apell;
+    } 
+    public setNombre(nom: string): void {
+        this.nombre = nom;
+    }
+    
+    public setApellido(apell : string): void {
         this.apellido = apell;
     }
+    
+   
+    public getNombre() : string {
+        return this.nombre;
+    }
+    
+    public getApellido() : string {
+        return this.apellido;
+    }
+    
+    
 }
+    
+
 export class Trago {
-    nombre: string;
-    color: string;
-    cantidad: number;
-    constructor(nom: string,col:string,cant: number){
+    private nombre: string;
+    private color: string;
+    private cantidad: number;
+    constructor(nom: string, col: string, cant: number){
         this.nombre = nom;
         this.color = col;
         this.cantidad = cant;
     }
+    public setNombre(nom : string): void{
+        this.nombre = nom;
+    }
+    
+    public setColor(col : string): void {
+        this.color = col;
+    }
+    
+    public setCantidad(cant : number): void {
+        this.cantidad= cant;
+    }
+    
+    
+     
+    public getNombre() : string {
+        return this.nombre;
+    }
+    
+    public getColor() : string {
+        return this.color;
+    }
+    
+    public getCantidad() : number {
+        return this.cantidad;
+    }
+    
+    
+   
+        
 }
+
 /**
  * Concepto: Los atributos son las caracteristicas individuales que diferencian un objeto  de otro y determinan su
  * apariencia, estado u otras cualidades. Los atributos se guardan en variables denominadas de instancia, y  cada 
@@ -41,6 +120,44 @@ let bar2: Bar = new Bar("el patio",200);
 let barman1 :BarMan = new BarMan("julio","perez");
 
 let trago1 : Trago = new Trago("mojito","rojo",1);
+
+
+
+
+bar1.setNombre("El pueblito");
+// bar1.nombre = "pueblito"; forma incorrecta actualizar un atributo
+
+console.log(bar1);
+// console.log(bar1.nombre);//forma incorrecta de pedir valor
+
+console.log(bar1.getNombre());
+bar1.setCapacidad(70);
+console.log(bar1.getCapacidad());
+
+
+
+
+barman1.setNombre("Carlos");
+console.log(barman1.getNombre());
+barman1.setApellido("Alegre");
+console.log(barman1.getApellido());
+
+
+
+
+trago1.setNombre("jugo");
+console.log(trago1.getNombre());
+trago1.setColor("azul");
+console.log(trago1.getColor());
+trago1.setCantidad(7);
+console.log(trago1.getCantidad());
+
+
+
+
+
+
+
 
 console.log(bar1);
 console.log(bar2);
